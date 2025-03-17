b2d6d6004b828e58672e4759c5fac9a04658aa57
Nicholas Dhanraj <nickjob_dhanraj@hotmail.com>
2025-03-16 20:22:51 -0400

    Update email.ts

--- app/utils/email.ts
+++ app/utils/email.ts
@@ -5,7 +5,7 @@ export function sendEmail(data: FormData) {
 
   fetch(apiEndpoint, {
     method: "POST",
-    body: JSON.(data),
+    body: JSON.data,
   })
     .then((res) => res.json())
     .then((response) => {
