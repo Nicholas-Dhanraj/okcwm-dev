3983827917089352dbb8566cc063440311ddd914
Nicholas Dhanraj <nickjob_dhanraj@hotmail.com>
2025-03-16 20:38:37 -0400

    Update route.ts

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -45,8 +45,8 @@ export async function POST(request: NextRequest) {
 
   try {
     await sendMailPromise();
-    return NextResponse.json({ message: "Email sent" });
+    // return NextResponse.json({ message: "Email sent" });
   } catch (err) {
-    return NextResponse.json({ error: err }, { status: 500 });
+    // return NextResponse.json({ error: err }, { status: 500 });
   }
 }
