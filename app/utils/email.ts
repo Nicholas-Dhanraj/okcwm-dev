ced442a7815d8390d4c1d4339a7d9b3410581fab
Nicholas Dhanraj <nickjob_dhanraj@hotmail.com>
2025-03-16 20:03:31 -0400

    Update email.ts

--- app/utils/email.ts
+++ app/utils/email.ts
@@ -2,17 +2,16 @@ import { FormData } from "../_components/contactForm";
 
 export function sendEmail(data: FormData) {
   const apiEndpoint = "/api/email";
-  console.log(data)
+
   fetch(apiEndpoint, {
     method: "POST",
     body: JSON.stringify(data),
-    // headers: { "Content-Type": "application/json", Accept: "application/json" },
   })
-    // .then((res) => res.json())
-    // .then((response) => {
-    //   alert(response.message);
-    // })
-    // .catch((err) => {
-    //   alert(err);
-    // });
+    .then((res) => res.json())
+    .then((response) => {
+      alert(response.message);
+    })
+    .catch((err) => {
+      alert(err);
+    });
 }
