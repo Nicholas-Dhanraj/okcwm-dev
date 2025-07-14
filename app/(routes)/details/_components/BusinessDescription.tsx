5c90fdd770fe7fe4b59767db8fedc23e98865f67
nicholas-dhanraj <nickjob_dhanraj@hotmail.com>
2025-07-14 11:24:21 -0400

    free payment fix

--- app/(routes)/details/_components/BusinessDescription.tsx
+++ app/(routes)/details/_components/BusinessDescription.tsx
@@ -54,7 +54,7 @@ function BusinessDescription({ business }: any) {
                       {
                         lineItems: [
                           {
-                            price: "price_1R4BFW03uwdOP6bvaQtdHymP",
+                            price: "price_1R4BFW03uwdOP6bvaQtdHymP_",
                             quantity: 1,
                           },
                         ],
